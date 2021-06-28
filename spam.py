4535a53387bc340a0ae4c033a1ff01133bc49d4b
himanshudas75 <himanshudas75@gmail.com>
2021-06-29 00:54:04 +0530

    s

--- spam.py
+++ spam.py
@@ -108,8 +108,7 @@ async def spam(ctx, arg0, arg1, arg2='100', arg3=0, arg4=0):
     if not arg2.isnumeric():    #DM Spam another server
         user_exist = False
         for user in guild.members:
-            print(user.id)
-            if str(user.id) == arg2[1:]:
+            if str(user) == arg2:
                 user_exist = True
                 break
         if not user_exist:
